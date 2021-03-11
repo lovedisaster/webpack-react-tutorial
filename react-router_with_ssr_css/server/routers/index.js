b5f9af5def0689a166619cd7d70bb4fd37c9004e
Nathan Zhang <nathan.zhang@carsales.com.auls>
2021-03-11 22:16:51 +1100

    fixed view file name

--- react-router_with_ssr_css/server/routers/index.js
+++ react-router_with_ssr_css/server/routers/index.js
@@ -5,7 +5,7 @@ var ReactDomString = require("../../dist/main-server");
 
 module.exports = function (app) {
       app.use("/home",function (req, res, next) {
-        res.render(config.paths.server_pages, { ReactDom : ReactDomString.default});
+        res.render("index", { ReactDom : ReactDomString.default});
         next();
       });
       return app;
